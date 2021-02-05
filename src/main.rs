69e67cd57862aa02628cd35d0b15f808cb291b07
nickschmitt <nickschmitt@gmail.com>
2021-02-04 22:45:09 -0800

    grrs initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+struct Cli {
+    /// The pattern to look for
+    pattern: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
 }
 
-let pattern = std::env::args().nth(1).expect("no pattern given");
-let path = std::env::args().nth(2).expect("no path given");
+
+fn main() {
+    let args = Cli::from_args();
+    let content = std::fs::read_to_string(&args.path)
+        .expect("could not read file");
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+}
+
